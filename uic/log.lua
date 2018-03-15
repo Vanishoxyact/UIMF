6d7fe8a228e5e163f222cbd81d13075eb34eb3fe
Alex-Guest <master_kenno@hotmail.co.uk>
2018-03-15 21:57:43 +0000

    Changes to Log.

--- uic/log.lua
+++ uic/log.lua
@@ -1,32 +1,23 @@
 local Log = {};
 
-Log.enableLog = false;
-Log.LogMode = true;
-
 Log.lines = {} --: vector<string>
 Log.maxLines = 1000;
-Log.filePath = "data/ui_log.txt";
+Log.filePath = "uimf_log.txt";
 
 --v function(str: string)
 function Log.write(str)
-    if Log.LogMode then
-        if #Log.lines > Log.maxLines then 
-            table.remove(Log.lines, 1);
-        end
-        
-        table.insert(Log.lines, str);
-
-        local file = io.open(Log.filePath,"w+");
-        for index, line in ipairs(Log.lines) do 
-            file:write(line.."\n");
-        end
-        
-        file:close();
-    else
-        local file = io.open(Log.filePath,"a+");
-        file:write(str.."\n");
-        file:close();
+    if #Log.lines > Log.maxLines then 
+        table.remove(Log.lines, 1);
     end
+    
+    table.insert(Log.lines, str);
+
+    local file = io.open(Log.filePath,"w+");
+    for index, line in ipairs(Log.lines) do 
+        file:write(line.."\n");
+    end
+    
+    file:close();
 end
 
 return {
