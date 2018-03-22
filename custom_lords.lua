d3556c614ce416d3fa7a34987f1b01e8da1039eb
Alex-Guest <master_kenno@hotmail.co.uk>
2018-03-22 21:33:33 +0000

    Updated custom lords poc. Added more CA commands.

--- custom_lords.lua
+++ custom_lords.lua
@@ -20,7 +20,7 @@ function createCustomLordFrame()
         princessButton:PositionRelativeTo(princeButton, buttonSize + gapSize, 0);
         princessButton.uic:SetState("active");
 
-        princeButton:RegisterForClick("princessButtonListener",
+        princeButton:RegisterForClick("princeButtonListener",
             function(context)
                 princessButton.uic:SetState("active");
             end
@@ -32,29 +32,55 @@ function createCustomLordFrame()
             end
         );
 
+        local defaultSkillsButton = TextButton.new("defaultSkillsButton", customLordFrame, "TEXT_TOGGLE", "Default");        
+        defaultSkillsButton:Resize(buttonSize, defaultSkillsButton.uic:Height());
+        defaultSkillsButton:PositionRelativeTo(princeButton, 0, 50);
+        defaultSkillsButton.uic:SetState("selected");
+
+        local magicSkillsButton = TextButton.new("magicSkillsButton", customLordFrame, "TEXT_TOGGLE", "Magic");
+        magicSkillsButton:Resize(buttonSize, magicSkillsButton.uic:Height());
+        magicSkillsButton:PositionRelativeTo(princessButton, 0, 50);
+        magicSkillsButton.uic:SetState("active");
+
+        defaultSkillsButton:RegisterForClick("defaultSkillsButtonListener",
+            function(context)
+                magicSkillsButton.uic:SetState("active");
+            end
+        );
+
+        magicSkillsButton:RegisterForClick("magicSkillsButtonListener",
+            function(context)
+                defaultSkillsButton.uic:SetState("active");
+            end
+        );
+
         local recuitButton = Button.new("recuitButton", customLordFrame, "CIRCULAR", "ui/campaign ui/edicts/lzd_alignment_of_building.png");
         local buttonWidth, buttonHeight = recuitButton.uic:Dimensions();
         recuitButton:PositionRelativeTo(customLordFrame, contentWidth/2 - buttonWidth/2, contentHeight - buttonHeight/2)
 
         local callBackFunction = function(context)
-            remove_all_units_from_character(get_character_by_cqi(context));
+            if defaultSkillsButton:IsSelected() then
+                cm:force_add_trait_on_selected_character("wh2_main_trait_hef_prince_melee");
+            else
+                cm:force_add_trait_on_selected_character("wh2_main_trait_hef_prince_magic");                
+            end
+
+            find_uicomponent(core:get_ui_root(), "units_panel", "main_units_panel", "units", "LandUnit 1"):SimulateLClick();
+            find_uicomponent(core:get_ui_root(), "units_panel", "main_units_panel", "button_group_unit", "button_disband"):SimulateLClick();
+            find_uicomponent(core:get_ui_root(), "dialogue_box", "both_group", "button_tick"):SimulateLClick();
+
             local generalButton = find_uicomponent(core:get_ui_root(), "layout", "info_panel_holder", "primary_info_panel_holder", "info_button_list", "button_general");
             generalButton:SimulateLClick();
             local renameButton = find_uicomponent(core:get_ui_root(), "character_details_panel", "background", "bottom_buttons", "button_rename");
             renameButton:SimulateLClick();
-            find_uicomponent(core:get_ui_root(), "character_details_panel"):SetVisible(false);
             find_uicomponent(core:get_ui_root(), "popup_text_input", "panel_title", "heading_txt"):SetStateText("Name your Lord");
+            find_uicomponent(core:get_ui_root(), "popup_text_input", "text_input_list_parent", "text_input1"):SetStateText("Name your Lord");
+            local textInput =  find_uicomponent(core:get_ui_root(), "popup_text_input", "text_input_list_parent", "text_input");
+            textInput:SimulateKey("a");
+            textInput:SimulateKey("b");
             local popupOkButton = find_uicomponent(core:get_ui_root(), "popup_text_input", "ok_cancel_buttongroup", "button_ok");
-            Util.registerForClick(popupOkButton, "popupOkButtonListener",
-                function(context)
-                    find_uicomponent(core:get_ui_root(), "character_details_panel"):SetVisible(true);
-                    cm:callback(
-                        function()
-                            find_uicomponent(core:get_ui_root(), "character_details_panel", "button_ok"):SimulateLClick();
-                        end, 0, "closeasdasd"
-                    );
-                end
-            );
+            popupOkButton:SimulateLClick();
+            find_uicomponent(core:get_ui_root(), "character_details_panel", "button_ok"):SimulateLClick();
         end
         
         recuitButton:RegisterForClick("recuitButtonListener",
@@ -62,16 +88,16 @@ function createCustomLordFrame()
                 if princeButton:IsSelected() then
                     cm:create_force_with_general(
                         "wh2_main_hef_eataine",
-                        "wh2_main_skv_inf_clanrats_0,wh2_main_skv_inf_clanrats_0,wh2_main_skv_inf_clanrats_0",
+                        "wh2_main_skv_inf_clanrats_0",
                         "wh2_main_eataine_lothern",
-                        210,
-                        280,
+                        217,
+                        273,
                         "general",
                         "wh2_main_hef_prince",
-                        "forname",
-                        "clan_name",
-                        "family_name",
-                        "other_name",
+                        "",
+                        "",
+                        "",
+                        "",
                         "my_custom_lord",
                         false,
                         callBackFunction
@@ -79,16 +105,16 @@ function createCustomLordFrame()
                 else
                     cm:create_force_with_general(
                         "wh2_main_hef_eataine",
-                        "wh2_main_skv_inf_clanrats_0,wh2_main_skv_inf_clanrats_0,wh2_main_skv_inf_clanrats_0",
+                        "wh2_main_skv_inf_clanrats_0",
                         "wh2_main_eataine_lothern",
-                        210,
-                        280,
+                        217,
+                        273,
                         "general",
                         "wh2_main_hef_princess",
-                        "forname",
-                        "clan_name",
-                        "family_name",
-                        "other_name",
+                        "",
+                        "",
+                        "",
+                        "",
                         "my_custom_lord",
                         false,
                         callBackFunction
@@ -129,13 +155,20 @@ function attachButtonToLordRecuitment()
             createCustomLordButton:RegisterForClick(
                 "createCustomLordButtonListener", 
                 function(context)
-                    output("Selected settlement:"..cm:get_campaign_ui_manager().settlement_selected);                    
+                    output("Selected settlement:"..cm:get_campaign_ui_manager().settlement_selected);  
+                    local region = string.sub(tostring(cm:get_campaign_ui_manager().settlement_selected), 12);
+                    output("Selected region:"..region);                      
+                    local settlement = get_region(region):settlement();
+                    output(settlement:logical_position_x());
+                    output(settlement:logical_position_y());
+                    
                     createCustomLordFrame();
                     characterPanel:SetVisible(false);
                     --find_uicomponent(core:get_ui_root(), "layout", "hud_center_docker"):SetVisible(false);
                     --find_uicomponent(core:get_ui_root(), "layout", "info_panel_holder"):SetVisible(false);
                 end
             );
+
         end,
         true
     );
@@ -143,4 +176,59 @@ end
 
 function custom_lords()
     attachButtonToLordRecuitment();
+    core:add_listener(
+        "char_panel_openeddfgfdg",
+        "PanelOpenedCampaign",
+        function(context) 
+            return context.string == "character_details_panel"; 
+        end,
+        function(context)
+            --cm:callback(function()
+                local chain = find_uicomponent(core:get_ui_root(), "character_details_panel", "background", "skills_subpanel", "listview", "list_clip", "list_box", "chain2", "chain");
+                if not not chain then
+                    local childCount = chain:ChildCount();
+                    for i=0, childCount-1  do
+                        local child = UIComponent(chain:Find(i));
+                        output(child:Id());
+                        --wh2_main_skill_innate_hef_prince_melee
+                        --wh2_main_skill_innate_hef_prince_magic
+                        
+                        local char = cm:get_campaign_ui_manager():get_char_selected();
+                        local cqi = string.sub(char, 15);
+                        output("cqi:"..cqi);
+                        local realChar = get_character_by_cqi(cqi);
+                        local id = child:Id();
+                        if id == "wh2_main_skill_hef_combat_graceful_strikes" 
+                        or id == "module_wh2_main_skill_hef_combat_graceful_strikes" 
+                        or id == "wh_main_skill_all_all_self_foe-seeker" 
+                        or id == "module_wh_main_skill_all_all_self_foe-seeker" 
+                        or id == "wh_main_skill_all_all_self_deadly_onslaught" then
+                            if not realChar:has_trait("wh2_main_trait_hef_prince_melee") then
+                                output("assdd");
+                                child:SetVisible(false);
+                                output("hid skill");
+                            else
+                                output("has skill melee");
+                            end
+                        end
+                        if id == "wh2_main_skill_all_magic_high_02_apotheosis" 
+                        or id == "module_wh2_main_skill_all_magic_high_02_apotheosis" 
+                        or id == "wh_main_skill_all_magic_all_06_evasion" 
+                        or id == "module_wh_main_skill_all_magic_all_06_evasion" 
+                        or id == "wh_main_skill_all_magic_all_11_arcane_conduit" then
+                            output("dfgfdg")
+                            if not realChar:has_trait("wh2_main_trait_hef_prince_magic") then
+                                output("dfgfdgdfg")
+                                child:SetVisible(false);
+                                output("hid skill");
+                            else
+                                output("has skill magic");
+                            end
+                        end
+                    end
+                end
+            --end, 1.0, "SADD");
+        end, 
+        true
+    );
 end
