885ef561be2dd2b7193b50c79878f3a76b3ff218
Alex-Guest <master_kenno@hotmail.co.uk>
2018-03-30 00:28:46 +0100

    Removed ref to other mods from UIMF

--- .vscode/entry.lua
+++ .vscode/entry.lua
@@ -1,6 +1,2 @@
 require("uimf_init");
 require("demo_uimf");
-require("other mods/custom_lords");
-require("other mods/trade_goods");
-require("other mods/improved_ruins");
-require("other mods/table_loading/table_loading");
