2ed14cfad9b94f59afb08f73bacf0d7c0500bb1d
Vanishoxyact <master_kenno@hotmail.co.uk>
2020-12-05 16:55:03 +0000

    Performance improvements to UIMF log

--- uic/log.lua
+++ uic/log.lua
@@ -1,24 +1,23 @@
 local Log = {};
 
-Log.lines = {} --: vector<string>
-Log.maxLines = 1000;
 Log.filePath = "uimf_log.txt";
+Log.fileReset = false;
 
 --v function(str: string)
 function Log.write(str)
     if write_log then
-        if #Log.lines > Log.maxLines then 
-            table.remove(Log.lines, 1);
-        end
-        
-        table.insert(Log.lines, str);
-
-        local file = io.open(Log.filePath,"w+");
-        for index, line in ipairs(Log.lines) do 
-            file:write(line.."\n");
-        end
-        
+        Log.resetFileIfNeeded();
+        local file = io.open(Log.filePath,"a+");
+        file:write(str.."\n");
         file:close();
+    end
+end
+
+function Log.resetFileIfNeeded()
+    if not Log.fileReset then
+        local file = io.open(Log.filePath,"w+");
+        file:close();
+        Log.fileReset = true;
     end
 end
 
